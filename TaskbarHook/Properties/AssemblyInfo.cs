279f70aeb64d876c19dbd74587d0f75375a0c505
Thomas Christof <thomas.christof@outlook.com>
2018-09-06 00:21:22 +0200

    Remove Nuget Package

--- TaskbarHook/Properties/AssemblyInfo.cs
+++ TaskbarHook/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TaskbarHook")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Indie-Dev")]
 [assembly: AssemblyProduct("TaskbarHook")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
